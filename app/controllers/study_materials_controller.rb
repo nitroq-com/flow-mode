45feb526e30c9c37f584f152fca189f73e0bc443
João Scotto <joao.scotto@gmail.com>
2025-03-09 16:27:44 -0300

    implement edit and delete for study materials

--- app/controllers/study_materials_controller.rb
+++ app/controllers/study_materials_controller.rb
@@ -1,10 +1,18 @@
 class StudyMaterialsController < ApplicationController
+  before_action :set_study_material, only: %i[show edit update destroy]
+
   def index
     @study_materials = StudyMaterial.all
   end
 
+  def show
+  end
+
   def new
     @study_material = StudyMaterial.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,11 +25,26 @@ class StudyMaterialsController < ApplicationController
     end
   end
 
+  def update
+    if @study_material.update(study_material_params)
+      redirect_to study_materials_path, notice: "Study material was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @study_material.destroy!
+    redirect_to study_materials_path, notice: "Study material was successfully destroyed."
+  end
+
   private
 
+  def set_study_material
+    @study_material = StudyMaterial.find(params[:id])
+  end
+
   def study_material_params
-    params
-      .require(:study_material)
-      .permit(:subject, :material_type, :description, :total_duration, :time_spent, :total_pages, :pages_read, :position, :resource_link)
+    params.require(:study_material).permit(:subject, :material_type, :description, :total_duration, :time_spent, :total_pages, :pages_read, :position, :resource_link)
   end
 end
--- app/models/study_material.rb
+++ app/models/study_material.rb
@@ -1,3 +1,10 @@
 class StudyMaterial < ApplicationRecord
   enum :material_type, [ :course, :ebook, :article, :video ]
+
+  def completed?
+    return true if self.pages_read.present? && self.pages_read >= self.total_pages.to_i
+    return true if self.time_spent.present? && self.time_spent >= self.total_duration.to_i
+
+    false
+  end
 end
